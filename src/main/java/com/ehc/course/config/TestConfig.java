98f3657325a37df997dffd17578f4c84154f947c
Ezequiel <ezequiel.henrique@gmail.com>
2019-11-25 21:38:38 -0300

    Order, Instant, ISO 8601 - part 1

--- src/main/java/com/ehc/course/config/TestConfig.java
+++ src/main/java/com/ehc/course/config/TestConfig.java
@@ -21,11 +21,11 @@ public class TestConfig implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "987654");
+		User u3 = new User(null, "Bob Gray", "bob@gmail.com", "16997854789", "159951");
+		User u4 = new User(null, "John Black", "john@gmail.com", "18998744588", "357753");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
 	}
 	
-	
-
 }
