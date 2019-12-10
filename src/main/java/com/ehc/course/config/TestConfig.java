c2e9c8129e3eb21f931a5a4a051b82f3d8600e2b
Ezequiel <ezequiel.henrique@gmail.com>
2019-12-10 18:40:33 -0300

    Payment, one-to-one associate

--- src/main/java/com/ehc/course/config/TestConfig.java
+++ src/main/java/com/ehc/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ehc.course.entities.Category;
 import com.ehc.course.entities.Order;
 import com.ehc.course.entities.OrderItem;
+import com.ehc.course.entities.Payment;
 import com.ehc.course.entities.Product;
 import com.ehc.course.entities.User;
 import com.ehc.course.entities.enums.OrderStatus;
@@ -87,6 +88,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
